03d813bea3e74423aa67f3c761162b594124c0da
Quentin Adeline <quentin.adeline18@gmail.com>
2023-03-06 16:12:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,70 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-#lire l'automates et retourner un tableau 
-def lire_automate(nom_fichier):
-    """
-    Lit un fichier texte décrivant un automate et renvoie un graphe NetworkX.
-    """
-    with open(nom_fichier, 'r') as f:
-        lines = f.readlines()
 
+#lire un fichier texte et renvoie un double tableau de la forme :
+def extraction_tableau(nom_fichier):
+    with open(nom_fichier, 'r') as f:
+        lines = f.readlines() # on lit le fichier
+
+    # Création du tableau vide
+    tab = [] 
+
+    # Ajout des nœuds et des transitions
+    for line in lines:
+        src, label, dst, io = line.split() # on récupère les informations de la ligne
+        tab.append([src, label, dst, io]) # on ajoute la transition
+
+    return tab
+
+# Affiche un tableau de transition avec en paramètre un double tableau de la forme :
+def affichage_automate_tableau(tab):
+    # Affichage du tableau
+    for i in range(len(tab)):
+        for j in range(len(tab[i])):
+            print(tab[i][j], end="\t") # on affiche la transition
+        print()
+
+#transforme un tableau de transition en graphe NetworkX
+def tableau_to_graphe(tab):
     # Création du graphe
     G = nx.DiGraph()
 
     # Ajout des nœuds et des transitions
-    for line in lines:
-        src, label, dst, io = line.split()
-        G.add_edge(src, dst, label=label)
+    for i in range(len(tab)):
+        src, label, dst, io = tab[i] # on récupère les informations de la ligne
+        G.add_edge(src, dst, label=label, io=io) # on ajoute la transition
 
-        if io == "I":
-            G.nodes[src]['color'] = 'green'
-        elif io == "O":
-            G.nodes[dst]['color'] = 'red'
+    # Définition des couleurs des nœuds si le nœud est d'entrée (=I) mettre en vert, si le nœud est de sortie (=O) mettre en rouge, sinon mettre en bleu
+    for n in G.nodes():
+        if n == "O":
+            G.nodes[n]['color'] = 'red' # on définit la couleur du nœud d'entrée
+        elif n == "I":
+            G.nodes[n]['color'] = 'green' # on définit la couleur du nœud de sortie
+        else:
+            G.nodes[n]['color'] = 'blue' # on définit la couleur des autres nœuds
 
     return G
 
-#afficher l'automate sous forme de tableau de transition avec les etiquettes en colonnes et les étates de transition en lignes exemple :
-#          a         b         
-#    0         1         2
-#    1         2         -
-#    2         0         1
-
-def affichage_automate_tableau(G):
-    """
-    Affiche un graphe NetworkX sous forme de tableau de transition.
-    """
-    # Récupération des états et des symboles
-    states = sorted(G.nodes())
-    symbols = sorted({d['label'] for u, v, d in G.edges(data=True)})
-
-    # Affichage du tableau
-    print(" " * 10, end="")
-    for symbol in symbols:
-        print(f"{symbol:10}", end="")
-    print()
-    for state in states:
-        print(f"{state:10}", end="")
-        for symbol in symbols:
-            try:
-                dst = next(v for u, v, d in G.edges(data=True) if u == state and d['label'] == symbol)
-                print(f"{dst:10}", end="")
-            except StopIteration:
-                print(f"{'-':10}", end="")
-        print()
-
-#afficher l'automate sous forme de graphe
+#Affiche un graphe NetworkX avec des couleurs différentes pour les nœuds d'entrée, de sortie et normaux.
 def affichage_automate_graphe(G):
-    """
-    Affiche un graphe NetworkX avec des couleurs différentes pour les nœuds d'entrée, de sortie et normaux.
-    """
-    # Définition des positions des nœuds
-    pos = nx.spring_layout(G)
-
     # Définition des couleurs des nœuds
-    node_colors = [G.nodes[n].get('color', 'blue') for n in G.nodes()]
-
-    # Dessin du graphe
-    nx.draw_networkx_nodes(G, pos, node_color=node_colors)
-    nx.draw_networkx_edges(G, pos)
-    nx.draw_networkx_labels(G, pos)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels={(u, v): d['label'] for u, v, d in G.edges(data=True)})
+    color_map = []
+    for n in G.nodes():
+        color_map.append(G.nodes[n]['color'])
 
     # Affichage du graphe
-    plt.axis('off')
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color=color_map, edge_color='black', width=1, alpha=0.7)
+    edge_labels = nx.get_edge_attributes(G, 'label')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
     plt.show()
 
+
 if __name__ == "__main__":
-    G = lire_automate("automate.txt")
-    affichage_automate_tableau(G)
-    affichage_automate_graphe(G)
-    
+    tableau = extraction_tableau("automate.txt") # on lit l'automate
+    affichage_automate_tableau(tableau) # on affiche le tableau
+    graphe = tableau_to_graphe(tableau) # on transforme le tableau en graphe
+    affichage_automate_graphe(graphe) # on affiche le graphe
+
+
