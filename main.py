acdd309ebc7eea29414971f3f43eedbbd81f70e4
Quentin Adeline <quentin.adeline18@gmail.com>
2023-03-19 22:59:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-
+#Fonction qui lit le fichier et qui renvoie un tableau de transition
 def lire_fichier_transition(nom_fichier):
     transitions = []
     with open(nom_fichier, 'r') as f:
         for ligne in f:
-            ligne = ligne.strip()
-            elems = ligne.split()
-            etat_type = elems[0]
-            etat_actuel = elems[1]
-            symbole = elems[2]
-            etat_cible = elems[3]
+            ligne = ligne.strip()           #ici on enlève les espaces
+            elems = ligne.split()           #ici on sépare les éléments
+            etat_type = elems[0]            #ici on récupère le type d'état
+            etat_actuel = elems[1]          #ici on récupère l'état actuel
+            symbole = elems[2]              #ici on récupère le symbole
+            etat_cible = elems[3]           #ici on récupère l'état cible
             transitions.append([etat_type, etat_actuel, symbole, etat_cible])
     return transitions
 
-def afficher_tableau_transition(tableau_transition):
+#Fonction qui affiche le tableau de transition
+#def afficher_tableau_transition(tableau_transition):
     print("Etat\tSymbole\tEtat cible")
     for transition in tableau_transition:
         print(transition[0], "\t", transition[1], "\t", transition[2], "\t", transition[3])
@@ -23,17 +24,17 @@ def afficher_tableau_transition(tableau_transition):
 def afficher_table_transition(transitions):
     # Création des entêtes
     entetes = ['I/O', 'Etat']
-    symboles = sorted(list(set([t[2] for t in transitions if t[2] != '-'])))
-    entetes.extend(symboles)
-    entete_str = '{:<6}{:<9}'.format(*entetes[:2])
+    symboles = sorted(list(set([t[2] for t in transitions if t[2] != '-']))) #ici on récupère les symboles
+    entetes.extend(symboles) #ici on ajoute les symboles à la liste des entêtes
+    entete_str = '{:<6}{:<9}'.format(*entetes[:2]) #ici on crée la première ligne du tableau
     for symbole in entetes[2:]:
-        entete_str += '{:<6}'.format(symbole)
+        entete_str += '{:<6}'.format(symbole) #ici on crée la deuxième ligne du tableau
     print(entete_str)
 
-    # Création des lignes pour chaque état
-    etats = sorted(list(set([t[1] for t in transitions])))
-    for etat in etats:
-        ligne = ['-'] * len(entetes)
+    # Création des lignes pour chaque état 
+    etats = sorted(list(set([t[1] for t in transitions]))) 
+    for etat in etats: 
+        ligne = ['-'] * len(entetes) 
         for t in transitions:
             if t[1] == etat:
                 # Indication d'entrée ou de sortie
@@ -55,13 +56,9 @@ def afficher_table_transition(transitions):
             ligne_str += '{:<6}'.format(ligne[i])
         print(ligne_str)
 
-
-
-# Transforme un tableau de transition de la fonction lire_fichier_transition("automate.txt") en graphe NetworkX 
-# On change aussi la couleur des nœuds d'entrée(I) en vert,de sortie (O) en rouge et normaux(-) en gris
 def tableau_to_graphe(tableau):
     G = nx.DiGraph()  # on crée un graphe orienté
-    for i in range(len(tableau)):
+    for i in range(len(tableau)): 
         if tableau[i][0] == "I":
             G.add_node(tableau[i][1], color="red")
         elif tableau[i][0] == "O":
@@ -72,8 +69,6 @@ def tableau_to_graphe(tableau):
             G.add_edge(tableau[i][1], tableau[i][3], label=tableau[i][2])
     return G
 
-
-# Affiche un graphe NetworkX avec matplotlib 
 def affichage_automate_graphe(G):
     # On récupère la liste des couleurs des nœuds
     node_colors = [G.nodes[node]['color'] for node in G.nodes]
@@ -83,17 +78,14 @@ def affichage_automate_graphe(G):
     pos = nx.spring_layout(G)
     # On affiche le graphe
     nx.draw(G, pos, with_labels=True, node_color=node_colors)
+    # On affiche les étiquettes des arcs
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    # On affiche le graphe
     plt.show()
-
-
-
-
 
 if __name__ == "__main__":
     tableau = lire_fichier_transition("automate.txt")  # on lit l'automate
-    afficher_tableau_transition(tableau)  # on affiche le tableau
-    print("\n")
-    afficher_table_transition(tableau)  # on affiche le tableau
+    #afficher_tableau_transition(tableau)  # on affiche le tableau
+    afficher_table_transition(tableau)  # on affiche le tableau de transition sous forme souhaitée
     graphe = tableau_to_graphe(tableau) # on transforme le tableau en graphe 
     affichage_automate_graphe(graphe) # on affiche le graphe
