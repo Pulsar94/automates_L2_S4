41c55b04f709e7c9100dd88e65cde565a127eba1
Pulsar94 <94862548+Pulsar94@users.noreply.github.com>
2023-03-26 23:01:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,16 +41,27 @@ import copy
 
 ############################################
 
-def safe_input(y, mini, maxi):
+def safe_input(y, mini1, maxi1, mini2, maxi2):
     while True:
         try:
             x = int(input(y))
-            while (x < mini or x > maxi):
-                x = int(input(
-                    "\n\n###############   Choisir un nombre entier entre "+mini+" et "+maxi+":    ################\n\n"))
+            while (x < mini1 or x > maxi1 or (x in (31, 32, 33, 34, 35))):
+                if mini2 =="" or maxi2 == "":
+                    x = int(input(
+                        "\n\n################   Choisir un nombre entier de {} à {} :   ################\n\n".format(mini1, maxi1)))
+                else:
+                    x = int(input(
+                        "\n\n################   Choisir un nombre entier de {} à {} et {} à {} :   ################\n\n".format(mini1, maxi1, mini2, maxi2)))
             break
         except ValueError:
-            print("\n\n########  S'il vous plait, entrer un entier entre "+mini+" et "+maxi+"  ########\n")
+            if mini2 == "" or maxi2 == "":
+                x = int(input(
+                    "\n\n################   S'il vous plait, entrer un entier situer de {} à {} :   ################\n\n".format(mini1,
+                                                                                                                 maxi1)))
+            else:
+                x = int(input(
+                    "\n\n################   S'il vous plait, entrer un entier situer de {} à {} et {} à {} :   ################\n\n".format(
+                        mini1, maxi1, mini2, maxi2)))
     return x
 
 
@@ -83,13 +94,146 @@ def generate_automate(number):
     ecriture_tableau(auto_temp, 'execution/Automate_' + str(number) + '-LanguageComplementaire.txt', "Automate Complémentarise: ")
 
 
+def menu_general():
+    # for cle, valeur in menu_dic.items():
+    #    print(str(cle) + " : " + str(valeur))
+
+    G = []
+    AutomataNb = 5
+    choice = 12
+    choice2 = 12
+
+    while G == []:
+        G = lire_fichier_transition("automates/automate"+str(AutomataNb)+".txt")
+
+    # Dictionnaire du menu principal
+    menu_dic_init = {1: "Afficher l'automate sous forme de tableau",
+                     2: "Afficher l'automate sous forme graphique",
+                     3: "Charger un automate",
+                     4: "Informations sur l'automate",
+                     5: "Opération sur l'automate",
+                     6: "Reconnaissance de mots",
+                     0: "Quitter le programme"}
+
+    # Dictionnaire du menu sur les opérations
+    menu_dic_ope = {1: "Afficher l'automate sous forme de tableau",
+                    2: "Standardisation",
+                    3: "Determination",
+                    4: "Compléter",
+                    5: "Minimisation",
+                    6: "Langage complementaire",
+                    0: "Retour au menu principal"}
+
+    while choice != 0: # Boucle du menu principal avec ses conditions
+        print("\n########################        Menu Principal        ########################\n")
+        print("L'automate chargé est l'automate n°{}\n".format(AutomataNb))
+
+        for i in range(7):
+            print(str(i) + " : " + menu_dic_init.get(i))
+        choice = safe_input("\nChoisir une fonction: ", 0, 6,"","")
+
+        if choice == 1: # Afficher l'automate sous forme de tableau
+            print("\n-------------- Table de transition de l'automate n°{} --------------".format(AutomataNb))
+            afficher_table_transition(G)
+            print("--------------------------------------------------------------------")
+
+        elif choice == 2: # Afficher l'automate sous forme graphique
+            Graphe = tableau_to_graphe(G)
+            affichage_automate_graphe(Graphe)
+
+        elif choice == 3: # Charger un automate
+            G = []
+
+            AutomataNb = safe_input("\nChoisir un automate situer de 1 à 30 et 36 à 44\n", 1, 30, 36, 44)
+            G = lire_fichier_transition("automates/automate" + str(AutomataNb) + ".txt")
+
+        elif choice == 4: # Informations sur l'automate
+            print("\n--------------------------------------------------------------------")
+            if automate_standard(G):
+                print("L'automate est standard")
+            else:
+                print("L'automate n'est pas standard")
+
+            if automate_determine(G):
+                print("L'automate est deterministe")
+            else:
+                print("L'automate n'est pas deterministe")
+
+            if verif_complet(G):
+                print("L'automate est complet")
+            else:
+                print("L'automate n'est pas complet")
+            print("--------------------------------------------------------------------")
+
+        elif choice == 5: # Opération sur l'automate
+
+            while choice2 != 0: ################# Boucle du menu secondaire avec ses conditions #################
+
+                print("\n########################    Opération sur les automates     ########################\n")
+                print("L'automate chargé est l'automate n°{}\n".format(AutomataNb))
+
+                for i in range(7):
+                    print(str(i) + " : " + menu_dic_ope.get(i))
+                choice2 = safe_input("\nChoisir une fonction: ", 0, 6, "", "")
+
+                if choice2 == 1: # Afficher l'automate sous forme de tableau
+                    print("\n-------------- Table de transition de l'automate n°{} --------------".format(AutomataNb))
+                    afficher_table_transition(G)
+                    print("--------------------------------------------------------------------")
+
+                elif choice2 == 2: # Standardisation
+                    G = lire_fichier_transition("automates/automate" + str(AutomataNb) + ".txt")
+                    G = standardiser_automate(G)
+                    print("\n--------------------- Automate n°{} standardisé ---------------------".format(AutomataNb))
+                    afficher_table_transition(G)
+                    print("-----------------------------------------------------------------------")
+
+                elif choice2 == 2: # Determination
+                    G = lire_fichier_transition("automates/automate" + str(AutomataNb) + ".txt")
+                    G = determiniser_automate(G)
+                    print("\n--------------------- Automate n°{} déterministe ---------------------".format(AutomataNb))
+                    afficher_table_transition(G)
+                    print("------------------------------------------------------------------------")
+
+                elif choice2 == 3: # Compléter
+                    G = lire_fichier_transition("automates/automate" + str(AutomataNb) + ".txt")
+                    G = complet(G)
+                    print("\n--------------------- Automate n°{} complet ---------------------".format(AutomataNb))
+                    afficher_table_transition(G)
+                    print("-------------------------------------------------------------------")
+
+                elif choice2 == 4: # Minimisation
+                    G = lire_fichier_transition("automates/automate" + str(AutomataNb) + ".txt")
+                    G = minimiser_automate(G)
+                    print("\n--------------------- Automate n°{} minimisé ---------------------".format(AutomataNb))
+                    afficher_table_transition(G)
+                    print("--------------------------------------------------------------------")
+
+                elif choice2 == 5: # Langage complementaire
+                    G = lire_fichier_transition("automates/automate" + str(AutomataNb) + ".txt")
+                    G = complementarisation_automate(G)
+                    print("\n-------------- Automate complémentaire de l'automate n°{}  --------------".format(AutomataNb))
+                    afficher_table_transition(G)
+                    print("---------------------------------------------------------------------------")
+
+                elif choice2 == 0: # Retour au menu principal
+                    print("Retour au menu principal...")
+
+            #################### Fin du menu secondaire ####################
+
+        elif choice == 6: # Reconnaissance de mots
+            print("Opération toujours en cours de développement")
+
+        elif choice == 0: # Quitter le programme
+            print("Merci de votre utilisation, à bientôt !")
+            return
+
+
+
 
 ################## Début du programme ##################
 
 if __name__ == "__main__":
-    G = []
-    menu = 0
-    AutomataNb = 5
 
     """
     Création du dossier 'execution' s'il n'existe pas pour stocker les fichiers des opération effectué sur l'automate
@@ -113,92 +257,4 @@ if __name__ == "__main__":
             continue
         generate_automate(i)
 
-    while G == []:
-        G = lire_fichier_transition("automates/automate"+str(AutomataNb)+".txt")
-
-    while (menu != 10):
-        print("""\n###################           Menu Principal           ###################
-            0. Afficher un automate sous forme de tableau
-            1. Afficher un automate sous forme graphique
-            2. Charger un automate
-            3. Informations sur l'automate
-            4. Standardisation
-            5. Determination       
-            6. Compléter 
-            7. Minimisation
-            8. Langage complementaire
-            9. Tester un mot
-            10. Quitter""")
-        menu = safe_input("\nChoisir une fonction: \n", 0, 10)
-
-        if menu == 0:
-            afficher_table_transition(G)
-
-        elif menu == 1:
-            Graphe = tableau_to_graphe(G)
-            affichage_automate_graphe(Graphe)
-
-        elif menu == 2:
-            G = []
-            AutomataNb = safe_input("\nChoisir un automate (1 à 30)\n", 1, 39)
-            G = lire_fichier_transition("automates/automate"+str(AutomataNb)+".txt")
-
-        elif menu == 3:
-            if automate_standard(G) :
-                print("L'automate est standard")
-            else:
-                print("L'automate n'est pas standard")
-
-            if automate_determine(G) :
-                print("L'automate est deterministe")
-            else:
-                print("L'automate n'est pas deterministe")
-
-            if verif_complet(G) :
-                print("L'automate est complet")
-            else:
-                print("L'automate n'est pas complet")
-
-        elif menu == 4:
-            G = standardiser_automate(G)
-            ## écriture du résultat dans le fichier txt
-            ecriture_tableau(G, 'execution/Automate_' + str(AutomataNb) + '-Standardise.txt', "Automate Standardise: ")
-            afficher_table_transition(G)
-            # fonction pour reconnaitre un mot
-
-        elif menu == 5:
-            G = determiniser_automate(G)
-            ## écriture du résultat dans le fichier txt
-            ecriture_tableau(G, 'execution/Automate_' + str(AutomataNb) + '-Determinise.txt', "Automate Determinise: ")
-            afficher_table_transition(G)
-            # fonction pour reconnaitre un mot
-
-        elif menu == 6:
-            G = complet(G)
-            ## écriture du résultat dans le fichier txt
-            ecriture_tableau(G, 'execution/Automate_' + str(AutomataNb) + '-Complet.txt', "Automate Complet: ")
-            afficher_table_transition(G)
-            # fonction pour reconnaitre un mot
-
-        elif menu == 7:
-            G = minimiser_automate(G)
-            ## écriture du résultat dans le fichier txt
-            ecriture_tableau(G, 'execution/Automate_' + str(AutomataNb) + '-Minimise.txt', "Automate Minimise: ")
-            afficher_table_transition(G)
-            # fonction pour reconnaitre un mot
-
-        elif menu == 8:
-            G = complementarisation_automate(G)
-            # écriture du résultat dans le fichier txt
-            ecriture_tableau(G, 'execution/Automate_' + str(AutomataNb) + '-LanguageComplementaire.txt', "Automate Complémentarise: ")
-            afficher_table_transition(G)
-            # fonction pour reconnaitre un mot
-
-        ## elif menu == 9:
-            # fonction pour reconnaitre un mot
-
-
-        else:
-            menu = 10
-            print("Merci de votre utilisation, à bientôt !")
-            break
+    menu_general()
